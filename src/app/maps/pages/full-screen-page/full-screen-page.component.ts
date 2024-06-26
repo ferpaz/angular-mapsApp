f68f99913ee9d68bc69f2430fc360abde5cd67e9
Fernando Paz <ferpaz64@gmail.com>
2024-06-26 14:12:15 +0200

    Ya se ubicó el mapa fullscreen en la primera ruta, se está usando el ID del div donde quedará el mapa, esto no debe usarse de esta manera

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'maps-full-screen-page',
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID  -- no deberia usarse con el id del div, ya veremos como arreglarlo
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-3.668081, 40.501955], // starting position [lng, lat]
+      zoom: 14, // starting zoom
+      accessToken: environment.mapbox_key, // Mapbox token
+    });
+  }
 
 }
