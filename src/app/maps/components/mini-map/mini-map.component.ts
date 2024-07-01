8ac2c55240a7d2ca18e0aa75ec693674d865e226
Fernando Paz <ferpaz64@gmail.com>
2024-07-01 18:54:36 +0200

    Se agrego la pagina de Propiedades para utilizar el mapa como imagenes estáticas

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'maps-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  @Input() lngLat?: [number, number];
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML map no fue encontrado';
+    if (!this.lngLat) throw 'No se ha proporcionado una coordenada para el mapa';
+
+    const map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false,
+    });
+
+    const marker = new Marker()
+      .setLngLat(this.lngLat)
+      .addTo(map);
+  }
 
 }
